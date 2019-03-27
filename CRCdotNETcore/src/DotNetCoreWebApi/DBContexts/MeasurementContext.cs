b2a3f93ec68f1f7b7849dcb842cce4a59d6eba97
AdamKoryczan <adamkoryczan@outlook.com>
2019-03-27 17:57:40 +0100

    Added DbContext

---
+++ CRCdotNETcore/src/DotNetCoreWebApi/DBContexts/MeasurementContext.cs
@@ -0,0 +1,19 @@
+﻿using DotNetCoreWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebApi.DBContexts
+{
+    public class MeasurementContext : DbContext
+    {
+        public MeasurementContext(DbContextOptions contextOptions) : base(contextOptions)
+        {
+
+        }
+
+        public DbSet<Measurement> Measurements { get; set; }
+    }
+}
